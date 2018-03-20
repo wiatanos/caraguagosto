97269ec55a68f7256a30edbd5fe1a7e3dda8662d
wiatanos <wiatan.o.s@outlook.com>
2018-03-20 17:30:33 -0300

    alterado links do menu

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<a class="navbar-brand" href="#">Menu</a>
+		<a class="navbar-brand">Caragua a Gosto</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
@@ -17,15 +17,15 @@
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav mr-auto">
 				<li class="nav-item active">
-					<a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+					<a class="nav-link" href="{{ url('/') }}">Inicio <span class="sr-only">(current)</span></a>
 				</li>
 				<li class="nav-item dropdown">
 					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 						Restaurantes <i class="fas fa-building"></i>
 					</a>
 					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-						<a class="dropdown-item" href="/restaurante.php">Cadastrar</a>
-						<a class="dropdown-item" href="#">Listar</a>
+						<a class="dropdown-item" href="{{ url('/') }}">Cadastrar</a>
+						<a class="dropdown-item" href="{{ url('restaurante/listar') }}">Listar</a>
 					</div>
 				</li>
 				<li class="nav-item dropdown">
@@ -33,9 +33,8 @@
 						Categorias <i class="fas fa-tags"></i>
 					</a>
 					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-						<a class="dropdown-item" href="#">Cadastrar</a>
-						<a class="dropdown-item" href="#">Editar</a>
-						<a class="dropdown-item" href="#">Listar</a>
+						<a class="dropdown-item" href="{{ url('categoria') }}">Cadastrar</a>
+						<a class="dropdown-item" href="{{ url('categoria/listar') }}">Listar</a>
 					</div>
 				</li>
 				<li class="nav-item dropdown">
@@ -43,8 +42,7 @@
 						Pratos <i class="fas fa-utensils"></i>
 					</a>
 					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-						<a class="dropdown-item" href="/prato.php">Cadastrar</a>
-						<a class="dropdown-item" href="#">Editar</a>
+						<a class="dropdown-item" href="{{ url('prato') }}">Cadastrar</a>
 						<a class="dropdown-item" href="#">Listar</a>
 					</div>
 				</li>
