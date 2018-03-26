2e300fee7dae23e31af2d3ba7f04608cebc88e5a
wiatanos <32138253+wiatanos@users.noreply.github.com>
2018-03-26 01:31:50 -0300

    fix style

--- assets/js/requests.js
+++ assets/js/requests.js
@@ -12,12 +12,14 @@ $('.btn').on('click', function(e){
 
   // pagination
 
-  $('body').on('click', 'a.page-link', function(event) {
-  	$.ajax({
-  		method: "get",
-  		url: page+'&id='+$('input[name="id"]').val(),
-  	})
-  	.done(function( msg ) {
-  		$('#resultado').html(msg);
-  	});
-  });	
+  // $('body').on('click', 'a.page-link', function(event) {
+  // 	$.ajax({
+  // 		method: "get",
+  // 		url: page+'&id='+$('input[name="id"]').val(),
+  // 	})
+  // 	.done(function( msg ) {
+  // 		$('#resultado').html(msg);
+  // 	});
+  // });
+
+  // restraurante > prato
