3430866f3db4843393ad0794b16e7a6fdb3d82a3
wiatanos <32138253+wiatanos@users.noreply.github.com>
2018-03-26 01:30:33 -0300

    fix insert, table de retorno, add funcao restaurante > prato

--- app/Http/Controllers/votacaoController.php
+++ app/Http/Controllers/votacaoController.php
@@ -11,9 +11,12 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Pagination\LengthAwarePaginator;
 
 use App\Http\Controllers\Controller;
-// use App\Http\Requests\categoriaValidator;
+use App\Http\Requests\votacaoValidator;
 use App\Models\Votacao;
 use App\Models\Restaurante;
+use App\Models\Pessoa;
+
+
 
 
 /**
@@ -25,14 +28,41 @@ class votacaoController extends BaseController
 	use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     public function index(){
-    	return view('votacao');
+        foreach (Restaurante::all() as $key => $value) {
+            $data['codigo'][$value->codigo] = $value->nome;
+        }
+
+        return view('votacao', compact('data'));
     }
 
-    public function insert(Request $request){
+    public function insert(votacaoValidator $request){
         try{
-            $votacao = new Votacao;
-            $votacao->fill($request->all());
-            $votacao->save();
+            $pessoa = Pessoa::where('cpf', $request->get('cpf'));
+            if ($pessoa->count() == 0) {
+                $pessoa = new Pessoa;
+                $pessoa->fill($request->all());
+                $pessoa->save();
+                $id = $pessoa->id;
+            }else{
+                $id = $pessoa->first()->id;
+            }
+
+            if(Votacao::where('pessoa_id', $id)->count() !== 0){
+                if (Votacao::where('pessoa_id', $id)->first()->restaurante_codigo == $request->get('restaurante_codigo')) {
+                    return redirect('votacao')->with('status', 'Você já votou nesse restaurente');
+                }
+            }
+
+            foreach ($request->get('prato_id') as $key => $value) {
+                $votacao = new Votacao;
+                $votacao->fill($request->except('prato_id'));
+                $votacao->prato_id = $value;
+                $votacao->apresentacao = $request->get('apresentacao')[$key];
+                $votacao->sabor = $request->get('sabor')[$key];
+                $votacao->pessoa_id = $id;
+                $votacao->save();
+
+            }
 
             return redirect('votacao')->with('status', 'Ae ae!');
         }catch (Exception $e){
@@ -41,26 +71,26 @@ class votacaoController extends BaseController
     }
 
     public function listar(Request $request){
-        $resultado = Votacao::where('restaurante_codigo', $request->get('id'))->paginate(1);
+        $resultado = Votacao::where('restaurante_codigo', $request->get('id'))->paginate(10);
 
         if($resultado->count() == 0){
             return '<div class="alert alert-warning">Nenhum voto contabilizado para tal restaurante</div>';
         };
 
-        $head = '<tr><td>Apresentação do Prato</td><td>Sabor do Prato</td><td>Ambiente</td></tr>';
+        $head = '<thead class="thead-dark"><tr><td>Prato</td><td>Apresentação</td><td>Sabor do Prato</td><td>Ambiente</td></tr><thead>';
 
         foreach ($resultado as $key => $value) {
-            $content[] = '<tr>
-            <td>'.$value->prato.'</td>
+            $content[] = '<tbody><tr>
+            <td>'.$value->prato->nome.'</td>
+            <td>'.$value->apresentacao.'</td>
             <td>'.$value->sabor.'</td>
             <td>'.$value->ambiente.'</td>
-            <tr>';
+            <tr></tbody>';
         }
-
         $comma_separated = implode("", $content);
 
 
-        return $data['table'] = '<table class="table">'.$head.$comma_separated.'</table><nav aria-label="Page navigation example">'.$resultado->links().'</nav>';
+        return $data['table'] = '<table class="table table-hover">'.$head.$comma_separated.'</table><nav aria-label="Page navigation example">'.$resultado->links().'</nav>';
     }
 
     public function relatorio(){
@@ -69,7 +99,15 @@ class votacaoController extends BaseController
             $data['codigo'][$value->codigo] = $value->nome;
         }
 
-        // return $data['codigo']
         return view('relatorio', compact('data'));
+    }
+
+    public function prato($id){
+
+        foreach (Restaurante::where('codigo', $id)->first()->pratos as $key => $value) {
+            $pratos[$value->id] = $value->nome;
+        }
+
+        return $pratos;
     }
 }
