0f05d5d5d641d74a0cfbd20a5db7cf3694495f32
wiatanos <32138253+wiatanos@users.noreply.github.com>
2018-03-26 01:28:54 -0300

    add pagination

--- resources/views/relatorio.blade.php
+++ resources/views/relatorio.blade.php
@@ -4,17 +4,19 @@
 
 @section('conteudo')
 <div class="row">
-	<div class="card col-12 mt-5">
-		<div class="card-header"><h5 class="text-center">Relatorio de Votações</h5></div>
-		<div class="card-body">
-			{{ Form::open(['url' => 'votacao/listar']) }}
-			<div class="form-group input-group">
-				<div class="input-group-addon">#</div>
-				{{ Form::select('id', $data['codigo'], null, ['class' => 'form-control ']) }}
-				{{ Form::button('Resultados', ['class' => 'btn btn-primary']) }}
+	<div class="col-12 mt-5">
+		<div class="card">
+			<div class="card-header"><h5 class="text-center">Relatorio de Votações</h5></div>
+			<div class="card-body">
+				{{ Form::open(['url' => 'votacao/listar', 'method' => 'post']) }}
+				<div class="form-group input-group">
+					<div class="input-group-addon">#</div>
+					{{ Form::select('id', $data['codigo'], null, ['class' => 'form-control ']) }}
+					{{ Form::button('Resultados', ['class' => 'btn btn-primary']) }}
+				</div>
+				{{ Form::close() }}
+				<div id="resultado" class="table-responsive"></div>
 			</div>
-			{{ Form::close() }}
-			<div id="resultado"></div>
 		</div>
 	</div>
 </div>
@@ -29,23 +31,27 @@
 
 	$(document).ready(function(){
 		$(document).bind('DOMNodeInserted', function(event){
-			var page = $(event.target).find('a').attr('href');
-			console.log($(event.target.nodeName))
-			$(event.target).find('li').addClass('page-item').find('span, a').addClass('page-link');
-			$(event.target).find('a').removeAttr('href');
+			var elemento = $(event.target);
+			$.each(elemento.find('a'), function(i, e){
+				$(e).attr('url', $(e).attr('href')+'&id='+$('select[name="id"]').val())
+			})
+			elemento.find('li').addClass('page-item').find('span, a').addClass('page-link');
+			elemento.find('a').removeAttr('href');
 		})
-	
-	})
 
-	$('body').on('click', 'a.page-link', function(event) {
+		$('body').on('click', 'a.page-link', function(event) {
 			$.ajax({
 				method: "get",
-				url: page+'&id='+$('input[name="id"]').val(),
+				url: $(this).attr('url'),
 			})
 			.done(function( msg ) {
 				$('#resultado').html(msg);
 			});
 		});
+
+	})
+
+	
 
 
 
